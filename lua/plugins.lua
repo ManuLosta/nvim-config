f6538539ae4c9095f6fdc4309e6599b49fd00a88
ManuLosta <mlostalom@gmail.com>
2023-12-31 20:00:24 -0300

    telescope config

---
+++ lua/plugins.lua
@@ -0,0 +1,60 @@
+return {
+  { 'folke/which-key.nvim', opts = {} },
+  -- Git related plugins
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
+
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Catpuccin Colorscheme 
+  {
+    "catpuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
+
+  -- Telescope plugin
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    }
+  },
+
+  -- Lualin statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = "catppuccin",
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+}
