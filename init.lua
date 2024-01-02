2a98261d9bda5208bebac30c8bf16d69c34ba62c
ManuLosta <mlostalom@gmail.com>
2024-01-01 23:55:36 -0300

    fix issues

--- init.lua
+++ init.lua
@@ -5,12 +5,12 @@ vim.g.mapleader = " "
 vim.wo.number = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -23,17 +23,23 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
+-- tabs & indentation
+vim.o.tabstop = 2 -- 2 spaces for tabs (prettier default)
+vim.o.shiftwidth = 2 -- 2 spaces for indent width
+vim.o.expandtab = true -- expand tab to spaces
+vim.o.autoindent = true -- copy indent from current line when starting new one
 
 require("config.lazy")
 require("config.keymaps")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,72 +1,55 @@
 return {
-  { 'folke/which-key.nvim',  opts = {} },
-  -- Git related plugins
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
+	{ "folke/which-key.nvim", opts = {} },
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
 
-  -- Detect tabstop and shiftwidth automatically
-  "tpope/vim-sleuth",
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
 
-  { 'numToStr/Comment.nvim', opts = {} },
+	{ "numToStr/Comment.nvim", opts = {} },
 
-  -- Catpuccin Colorscheme
-  {
-    "catpuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end,
-  },
+	-- Catpuccin Colorscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 
-  -- Lualine statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = "catppuccin",
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+	-- Lualine statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = "catppuccin",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
 
-  -- Autoformatting plugin
-  {
-    "stevearc/conform.nvim",
-    opts = {},
-  },
+	-- Autoformatting plugin
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
 
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {},
-  }
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
 }
